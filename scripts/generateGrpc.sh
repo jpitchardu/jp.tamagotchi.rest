6f986e2f228567e81399c9daa56ed2368212ba1f
J. Pichardo <j.pichardo@tuta.io>
2018-06-05 04:37:34 -0700

    grpc

--- scripts/generateGrpc.sh
+++ scripts/generateGrpc.sh
@@ -1,4 +1,5 @@
 TOOLS_PATH='/home/jpichardo/.nuget/packages/grpc.tools/1.9.0-pre2/tools/linux_x64';
 
-
-$TOOLS_PATH/protoc -I ../jp.tamagotchi.proto.business/service/ --csharp_out jp.tamagotchi.shared ../Shared --grpc_out jp.tamagotchi.shared --plugin=protoc-gen-grpc=$TOOLS_PATH/grpc_csharp_plugin
+for i in `find ./jp.tamagotchi.proto.business/service -name '*.proto'` ; 
+do $TOOLS_PATH/protoc -I ./ --csharp_out ./Services $i --grpc_out ./Services --plugin=protoc-gen-grpc=$TOOLS_PATH/grpc_csharp_plugin ;
+done
